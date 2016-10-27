ffbaa79b8a851f0c2f6aa65463da5bbca4f867ea
Benjamin Allgeier <BenAndChris@benjamins-macbook.local>
2016-10-27 16:02:54 -0500

    This time I am committing the 2 Photo+ files.

---
+++ Photorama/Photo+CoreDataClass.swift
@@ -0,0 +1,37 @@
+//
+//  Photo+CoreDataClass.swift
+//  Photorama
+//
+//  Created by Benjamin Allgeier on 10/20/16.
+//  Copyright © 2016 ballgeier. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+import CoreData
+
+
+public class Photo: NSManagedObject {
+    
+    // Insert code here to add functionality to your managed object subclass
+    
+    var image: UIImage?
+    
+    override public func awakeFromInsert() {
+        super.awakeFromInsert()
+//        print(#function)
+        
+        // since calling the save() function, only get an error if don't 
+        // have photoKey initialized. The others are getting initialized 
+        // in FlickrAPI's photoFromJSONObject via context's perform and wait
+                
+        // Give the properties their initial values
+//        title = ""
+//        photoID = ""
+//        remoteURL = URL(fileURLWithPath: "") // can't give no argument
+        photoKey = UUID().uuidString
+        timesViewed = 0
+//        dateTaken = Date()
+    } // end awakeFromInsert()
+
+} // end class Photo
---
+++ Photorama/Photo+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Photo+CoreDataProperties.swift
+//  Photorama
+//
+//  Created by Benjamin Allgeier on 10/27/16.
+//  Copyright © 2016 ballgeier. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Photo {
+
+    // came from template -- note that it is public
+//    @nonobjc public class func fetchRequest() -> NSFetchRequest<Photo> {
+//        return NSFetchRequest<Photo>(entityName: "Photo");
+//    }
+
+    @NSManaged public var dateTaken: Date
+    @NSManaged public var photoID: String
+    @NSManaged public var photoKey: String
+    @NSManaged public var remoteURL: URL
+    @NSManaged public var title: String
+    @NSManaged public var timesViewed: Int64
+
+}
